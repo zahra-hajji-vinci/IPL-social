43849126f5079394ae301ec0cafb3ce9b152144e
zahra-hajji-vinci <zahra.hajji@student.vinci.be>
2025-01-17 16:23:26 +0100

    Add PasswordChecker initial test setup

---
+++ spec/appSpec.js
@@ -0,0 +1,8 @@
+import { PasswordChecker } from "../src/passwordChecker.js";
+
+describe("PasswordChecker", function () {
+    // Simple demonstration test to verify the test setup is working
+    it("demo", function(){  
+        expect(1).toBe(1);
+    });
+});
