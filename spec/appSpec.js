b08eb14ca031421faf9c6164815f56706e5ab487
zahra-hajji-vinci <zahra.hajji@student.vinci.be>
2025-01-17 16:50:03 +0100

    Update PasswordChecker responses for clarity in validation messages

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -7,7 +7,7 @@ describe("Mininum 8 character", function () {
 
         let result = new PasswordChecker().minimum8Characters(password);
 
-        expect(result).toBe("good");
+        expect(result).toBe("good length");
     });
 
     it("should return 'bad' when the password is < 8", function () {    
@@ -15,18 +15,26 @@ describe("Mininum 8 character", function () {
 
         let result = new PasswordChecker().minimum8Characters(password);
 
-        expect(result).toBe("bad");
+        expect(result).toBe("bad length");
     });
 });
 
 describe("Special character", function () { 
 
-    it("should return 'good' when the password contains special character", function () {
+    it("should return 'countain special character' when the password contains special character", function () {
         const password = "password@";
 
         let result = new PasswordChecker().specialCharacter(password);
 
-        expect(result).toBe("good");
+        expect(result).toBe("countain special character");
+    });
+
+    it("should return 'does not countain special character' when the password does not contain special character", function () {
+        const password = "password";
+
+        let result = new PasswordChecker().specialCharacter(password);
+
+        expect(result).toBe('does not countain special character');
     });
 
 });
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,12 +1,15 @@
 export class PasswordChecker {
 
     minimum8Characters(password) {
-        return password.length >= 8 ? "good" : "bad";
+        return password.length >= 8 ? "good length" : "bad length";
     }
 
     specialCharacter(password) {
         if (password.match(/[!@#$%^&*(),.?":{}|<>]/)) {
-            return "good";
+            return "countain special character";
+        }
+        if (!password.match(/[!@#$%^&*(),.?":{}|<>]/)) {
+            return "does not countain special character";
         }
     }
 
