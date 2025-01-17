85372ebb5b9babb9944d496044e8a2d86550e51c
zahra-hajji-vinci <zahra.hajji@student.vinci.be>
2025-01-17 16:37:46 +0100

    Implement minimum password length check in PasswordChecker

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,8 +1,13 @@
 import { PasswordChecker } from "../src/passwordChecker.js";
 
 describe("PasswordChecker", function () {
-    // Simple demonstration test to verify the test setup is working
-    it("demo", function(){  
-        expect(1).toBe(1);
+   
+    it("should return 'good' when the password is >= 8", function () {
+        const password = "password";
+
+        let result = new PasswordChecker().minimum8Characters(password);
+
+        expect(result).toBe("good");
     });
+
 });
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,3 +1,11 @@
+
 export class PasswordChecker {
+
+    minimum8Characters(password) {
+        if (password.length >= 8) {
+            return "good";
+        }
+    }
+
 
 }
